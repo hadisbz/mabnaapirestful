5bb31bfe9a0e48122500afdffd7bbfabbe2419cc
Azade <azadehhajmanochehry@yahoo.com>
2022-01-25 10:48:23 +0330

    Trade inserted

---
+++ src/main/java/ir/dotin/bigdata/project/mabnaapirestful/model/exchange/TradesModel.java
@@ -0,0 +1,70 @@
+package ir.dotin.bigdata.project.mabnaapirestful.model.exchange;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import ir.dotin.bigdata.project.mabnaapirestful.api.response.MetaResponse;
+import ir.dotin.bigdata.project.mabnaapirestful.api.response.exchange.InstrumentsResponse;
+import ir.dotin.bigdata.project.mabnaapirestful.model.MetaModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(schema = "public", name = "trades")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TradesModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "local_id")
+    private Long localId;
+
+    private String id;
+
+    @Column(name="instrument_id")
+    private String instrumentId;
+
+    @Column(name="date_time")
+    private String dateTime;
+
+    @Column(name="open_price")
+    private Double openPrice;
+
+    @Column(name="high_price")
+    private Double highPrice;
+
+    @Column(name="low_price")
+    private Double lowPrice;
+
+    @Column(name="real_close_price")
+    private Double realClosePrice;
+
+    @Column(name="real_close_price_change")
+    private Double realClosePriceChange;
+
+    @Column(name="close_price")
+    private Double closePrice;
+
+    @Column(name="close_price_change")
+    private Double closePriceChange;
+
+    @Column(name="buyer_count")
+    private Integer buyerCount;
+
+    private Long volume;
+
+    private Double value;
+
+    @Column(name="trade_count")
+    private Integer tradeCount;
+
+    @Column(name="adjusted_close_price")
+    private Double adjustedClosePrice;
+
+    @Embedded
+    private MetaModel meta;
+
+
+}
